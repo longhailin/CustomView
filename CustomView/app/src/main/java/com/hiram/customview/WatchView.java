f36f3f9db21f71c50994570816c76a6406cd31e3
long <1065207999@qq.com>
2017-03-29 12:55:50 +0800

    自定义控件

--- CustomView/app/src/main/java/com/hiram/customview/WatchView.java
+++ CustomView/app/src/main/java/com/hiram/customview/WatchView.java
@@ -63,15 +63,15 @@ public class WatchView extends View {
     }
 
     protected void drawPoints(Canvas canvas, int len){
-//􀝸􃧧􀨆􃌫􃔏􁰦􄰤
         calendar.setTimeInMillis(System.currentTimeMillis());
-//􃧧􀨆􁰦􀠶􃀂
         int hours = calendar.get(Calendar.HOUR) % 12;//􄖜􁦒􀑪 12 􁈿􁰦􀡦
         int minutes = calendar.get(Calendar.MINUTE);
         int seconds = calendar.get(Calendar.SECOND);
         //花时针
         int degree = 360 / 12 * hours;
-        double radians = Math.toRadians(degree);
+
+        int temp = 360 / 60 * minutes / 12;
+        double radians = Math.toRadians(degree + temp);
         int r = len / 2;
         int startX = r;
         int startY = r;
@@ -79,36 +79,27 @@ public class WatchView extends View {
         int endY = (int) (startY + r * 0.5 * Math.sin(radians));
         canvas.save();
         paint.setStrokeWidth(3);
-//0 􁓖􀓾 3 􂛩􀼴􁔰􀿻􀋈􁰦􄰤􀓾 12 􂛩􀼴􁔰􀿻􀋈􁡰􀔕􄴰􃾱􁈶􂭫􁐳􁯻􄖜 90 􁓖
         canvas.rotate(-90, r, r);
-//􂭫􁰦􄪸
         canvas.drawLine(startX, startY, endX, endY, paint);
         canvas.restore();
-//􂭫􀠶􄪸
-//􄇑􃇇􄀂􁓖
         degree = 360 / 60 * minutes;
-        radians = Math.toRadians(degree);
+        temp = 360 / 60 * seconds / 60;
+        radians = Math.toRadians(degree + temp);
         endX = (int) (startX + r * 0.6 * Math.cos(radians));
         endY = (int) (startY + r * 0.6 * Math.sin(radians));
         canvas.save();
         paint.setStrokeWidth(2);
-//0 􁓖􀓾 3 􂛩􀼴􁔰􀿻􀋈􁰦􄰤􀓾 12 􂛩􀼴􁔰􀿻􀋈􁡰􀔕􄴰􃾱􁈶􂭫􁐳􁯻􄖜 90 􁓖
         canvas.rotate(-90, r, r);
-//􂭫􁰦􄪸
         canvas.drawLine(startX, startY, endX, endY, paint);
         canvas.restore();
-//􂭫􃀂􄪸
         degree = 360 / 60 * seconds;
         radians = Math.toRadians(degree);
         endX = (int) (startX + r * 0.8 * Math.cos(radians));
         endY = (int) (startY + r * 0.8 * Math.sin(radians));
         canvas.save();
         paint.setStrokeWidth(1);
-//0 􁓖􀓾 3 􂛩􀼴􁔰􀿻􀋈􁰦􄰤􀓾 12 􂛩􀼴􁔰􀿻􀋈􁡰􀔕􄴰􃾱􁈶􂭫􁐳􁯻􄖜 90 􁓖
         canvas.rotate(-90, r, r);
-//􂭫􁰦􄪸
         canvas.drawLine(startX, startY, endX, endY, paint);
-//􀞽􃔉􃀂􄪸􂭫􀑚“􁉮􁐤”
         radians = Math.toRadians(degree - 180);
         endX = (int) (startX + r * 0.15 * Math.cos(radians));
         endY = (int) (startY + r * 0.15 * Math.sin(radians));
